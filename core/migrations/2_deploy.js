2af877b54924d5ecf7be77b9fbab9f6e42d5e68a
deployAt <mikowhy@tutanota.com>
2020-11-19 12:19:33 +0100

    feat: deploy uniswap core contracts

--- core/migrations/2_deploy.js
+++ core/migrations/2_deploy.js
@@ -1,4 +1,6 @@
 const Factory = artifacts.require('UniswapV2Factory')
+const Token1 = artifacts.require('Token1')
+const Token2 = artifacts.require('Token2')
 
 module.exports = async function (deployer, network, addresses) {
   await deployer.deploy(Factory, addresses[0])
@@ -10,7 +12,13 @@ module.exports = async function (deployer, network, addresses) {
     token1Address = ''
     token2Adress = ''
   } else {
+    await deployer.deploy(Token1)
+    await deployer.deploy(Token2)
+    const token1 = await Token1.deployed()
+    const token2 = await Token2.deployed()
+    token1Address = token1.address
+    token2Address = token2.address
   }
 
-  await factory.createPair()
+  await factory.createPair(token1Address, token2Address)
 }
